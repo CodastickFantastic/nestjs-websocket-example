528b24cb2e9bfc4fffa04c9d3a2c2c52a0801154
Jakub Wojtysiak <it.jakub.wojtysiak@gmail.com>
2024-08-08 12:01:57 +0200

    Connected NestJS to MongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
@@ -9,6 +10,7 @@ import { ConfigModule } from '@nestjs/config';
       envFilePath: '.env',
       isGlobal: true
     }),
+    MongooseModule.forRoot(`mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@localhost:${process.env.MONGO_PORT}/${process.env.MONGO_INTIDB_DATABASE}`),
   ],
   controllers: [AppController],
   providers: [AppService],
