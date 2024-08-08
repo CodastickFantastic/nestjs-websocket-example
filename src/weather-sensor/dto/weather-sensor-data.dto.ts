3e1878dbbeb3fd605cc3a0a9fbb30a2f461c6beb
Jakub Wojtysiak <it.jakub.wojtysiak@gmail.com>
2024-08-08 14:49:11 +0200

    Rebuilded DTO

--- src/weather-sensor/dto/weather-sensor-data.dto.ts
+++ src/weather-sensor/dto/weather-sensor-data.dto.ts
@@ -5,13 +5,17 @@ enum TemperatureUnit {
     "F"
 }
 export class CreateSensorDataDTO {
+
     @IsString({ message: "Provide valid sensor_id (string)" })
     sensor_id: string;
+
     @IsString({ message: "Provide valid temperatureUnit (C or F)" })
     @IsEnum(TemperatureUnit, { message: "Provide valid temperatureUnit (C or F)" })
     temperatureUnit: string
+
     @IsNumber({}, { message: "Provide valid temperature (number)" })
     temperature: number
+
     @IsNumber({}, { message: "Provide valid humidity (number betwen 0 and 100 )" })
     @Min(0, { message: "Humidity cannot be less than 0" })
     @Max(100, { message: "Humidity cannot be more than 100" })
