a9fd04c622adfb2e6aa38bb7863c8044b488fc58
Jakub Wojtysiak <it.jakub.wojtysiak@gmail.com>
2024-08-09 11:22:45 +0200

    Added Create method to Weather Service

--- src/weather-sensor/weather-sensor.module.ts
+++ src/weather-sensor/weather-sensor.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { WeatherSensorGateway } from './weather-sensor.gateway';
 import { WeatherSensorService } from './weather-sensor.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { SensorData, SensorDataSchema } from './schema/sensor-data.shema';
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: SensorData.name, schema: SensorDataSchema }])],
   providers: [WeatherSensorGateway, WeatherSensorService]
 })
 export class WeatherSensorModule { }
--- src/weather-sensor/weather-sensor.service.ts
+++ src/weather-sensor/weather-sensor.service.ts
@@ -1,4 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { CreateSensorDataDTO } from './dto/weather-sensor-data.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { SensorData } from './schema/sensor-data.shema';
+import { Model } from 'mongoose';
+import { WsException } from '@nestjs/websockets';
 
 @Injectable()
-export class WeatherSensorService {}
+export class WeatherSensorService {
+    constructor(@InjectModel(SensorData.name) private model: Model<SensorData>) { }
+
+
+    async create(data: CreateSensorDataDTO) {
+        try {
+            let { sensor_id, temperatureUnit, temperature, humidity } = data
+
+            const newSensorData = new this.model({
+                sensor_id,
+                temperatureUnit,
+                temperature,
+                humidity
+            })
+
+            let sensorData = await newSensorData.save()
+            return { event: "message", data: sensorData };
+
+        } catch (error) {
+            throw new WsException(`DB Create Error:${error.message}`);
+        }
+    }
+}
