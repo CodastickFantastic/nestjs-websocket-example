2e2dbd2fce51d43cc8fed5663de2a4be12ed4b93
Jakub Wojtysiak <it.jakub.wojtysiak@gmail.com>
2024-08-09 10:43:02 +0200

    Created Data Schema

---
+++ src/weather-sensor/schema/sensor-data.shema.ts
@@ -0,0 +1,21 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { HydratedDocument } from "mongoose";
+
+export type SensorDataDocument = HydratedDocument<SensorData>
+
+@Schema({ timestamps: true })
+export class SensorData {
+    @Prop({ required: true })
+    sensor_id: string
+
+    @Prop({ required: true, enum: ['C', 'F'] })
+    temperatureUnit: string
+
+    @Prop({ required: true })
+    temperature: number
+
+    @Prop({ required: true, min: 0, max: 100 })
+    humidity: number
+}
+
+export const SensorDataSchema = SchemaFactory.createForClass(SensorData)
