81ceb4b35e0fd588ebad17c8de86919eb7880410
Jakub Wojtysiak <it.jakub.wojtysiak@gmail.com>
2024-08-09 17:07:11 +0200

    Switched to simple MONGO_URI env

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,10 +8,9 @@ import { WeatherSensorModule } from './weather-sensor/weather-sensor.module';
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: '.env',
       isGlobal: true
     }),
-    MongooseModule.forRoot(`mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@localhost:${process.env.MONGO_PORT}/${process.env.MONGO_INTIDB_DATABASE}`),
+    MongooseModule.forRoot(process.env.MONGO_URI),
     WeatherSensorModule,
   ],
   controllers: [AppController],
